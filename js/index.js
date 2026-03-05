e8398d51d608a020d1c837acd1c223cf5e5f096e
Jakob Lundqvist <jalu1000@stud.ek.dk>
2026-03-05 15:31:17 +0100

    forside

--- js/index.js
+++ js/index.js
@@ -12,3 +12,29 @@ fetch("https://kea-alt-del.dk/t7/api/categories")
       `;
     });
   });
+const sliderTrack = document.querySelector("#sliderTrack");
+
+fetch("https://kea-alt-del.dk/t7/api/products")
+  .then((response) => response.json())
+  .then((data) => {
+    const products = data.slice(0, 50);
+
+    products.forEach((product) => {
+      sliderTrack.innerHTML += `
+          <a href="product.html?id=${product.id}" class="slider-item">
+            <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"
+                 alt="${product.productdisplayname}">
+          </a>
+        `;
+    });
+
+    // Dupliker for infinite scroll
+    products.forEach((product) => {
+      sliderTrack.innerHTML += `
+          <a href="product.html?id=${product.id}" class="slider-item">
+            <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"
+                 alt="${product.productdisplayname}">
+          </a>
+        `;
+    });
+  });
